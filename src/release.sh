a32bb1a38fb1d87ac83bb8505c7b27078bf0b785
Ben Jefferies <benjefferies@echosoft.uk>
2019-09-12 11:35:57 +0100

    Remove references to settings.xml (#8)

--- src/release.sh
+++ src/release.sh
@@ -19,5 +19,5 @@ if [ -n "$1" ]
 then
      MAVEN_REPO_LOCAL="-Dmaven.repo.local=$1"
 fi
-mvn -s settings.xml $MAVEN_REPO_LOCAL -Dusername=$GITHUB_ACCESS_TOKEN release:prepare -B
-mvn -s settings.xml $MAVEN_REPO_LOCAL release:perform -B -Darguments="-Dmaven.javadoc.skip=true"
+mvn $MAVEN_REPO_LOCAL -Dusername=$GITHUB_ACCESS_TOKEN release:prepare -B
+mvn $MAVEN_REPO_LOCAL release:perform -B -Darguments="-Dmaven.javadoc.skip=true"
